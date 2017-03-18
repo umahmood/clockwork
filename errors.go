2064265509754934a72a0e48403cfff7a8d9321d
umahmood <umahmood@users.noreply.github.com>
2017-03-18 00:07:16 +0000

    Move error declarations into single var clause

--- errors.go
+++ errors.go
@@ -36,181 +36,183 @@ var (
 // Check the following link for more information:
 // https://www.clockworksms.com/doc/reference/faqs/api-error-codes/
 
-// ErrInternal internal error something went wrong in our API
-var ErrInternal = errors.New("clockwork: internal error")
+var (
+	// ErrInternal internal error something went wrong in our API
+	ErrInternal = errors.New("clockwork: internal error")
 
-// ErrInvalidUsernamePassword invalid username or password
-var ErrInvalidUsernamePassword = errors.New("clockwork: invalid username or password")
+	// ErrInvalidUsernamePassword invalid username or password
+	ErrInvalidUsernamePassword = errors.New("clockwork: invalid username or password")
 
-// ErrInsufficientCredit insufficient credits available
-var ErrInsufficientCredit = errors.New("clockwork: insufficient credits available")
+	// ErrInsufficientCredit insufficient credits available
+	ErrInsufficientCredit = errors.New("clockwork: insufficient credits available")
 
-// ErrAuthFail authentication failure
-var ErrAuthFail = errors.New("clockwork: authentication failure")
+	// ErrAuthFail authentication failure
+	ErrAuthFail = errors.New("clockwork: authentication failure")
 
-// ErrInvalidMsgType invalid 'MsgType' parameter
-var ErrInvalidMsgType = errors.New("clockwork: invalid 'MsgType'")
+	// ErrInvalidMsgType invalid 'MsgType' parameter
+	ErrInvalidMsgType = errors.New("clockwork: invalid 'MsgType'")
 
-// ErrMissingTo 'To' parameter not specified
-var ErrMissingTo = errors.New("clockwork: 'To' parameter not specified")
+	// ErrMissingTo 'To' parameter not specified
+	ErrMissingTo = errors.New("clockwork: 'To' parameter not specified")
 
-// ErrMissingContent 'Content' parameter not specified
-var ErrMissingContent = errors.New("clockwork: 'Content' parameter not specified")
+	// ErrMissingContent 'Content' parameter not specified
+	ErrMissingContent = errors.New("clockwork: 'Content' parameter not specified")
 
-// ErrMissingMessageID 'MessageID' parameter not specified
-var ErrMissingMessageID = errors.New("clockwork: 'MessageID' parameter not specified")
+	// ErrMissingMessageID 'MessageID' parameter not specified
+	ErrMissingMessageID = errors.New("clockwork: 'MessageID' parameter not specified")
 
-// ErrUnknownMessageID unknown 'MessageID'
-var ErrUnknownMessageID = errors.New("clockwork: unknown 'MessageID'")
+	// ErrUnknownMessageID unknown 'MessageID'
+	ErrUnknownMessageID = errors.New("clockwork: unknown 'MessageID'")
 
-// ErrInvalidTo invalid 'To' parameter
-var ErrInvalidTo = errors.New("clockwork: invalid 'To' parameter some or all numbers were not correct")
+	// ErrInvalidTo invalid 'To' parameter
+	ErrInvalidTo = errors.New("clockwork: invalid 'To' parameter some or all numbers were not correct")
 
-// ErrInvalidFrom invalid 'From' parameter
-var ErrInvalidFrom = errors.New("clockwork: invalid 'From' parameter")
+	// ErrInvalidFrom invalid 'From' parameter
+	ErrInvalidFrom = errors.New("clockwork: invalid 'From' parameter")
 
-// ErrMessageTooLong message text is too long
-var ErrMessageTooLong = errors.New("clockwork: message text is too long")
+	// ErrMessageTooLong message text is too long
+	ErrMessageTooLong = errors.New("clockwork: message text is too long")
 
-// ErrRoutingMessage cannot route message
-var ErrRoutingMessage = errors.New("clockwork: cannot route message")
+	// ErrRoutingMessage cannot route message
+	ErrRoutingMessage = errors.New("clockwork: cannot route message")
 
-// ErrMessageExpired message expired
-var ErrMessageExpired = errors.New("clockwork: message expired")
+	// ErrMessageExpired message expired
+	ErrMessageExpired = errors.New("clockwork: message expired")
 
-// ErrNoRoute no route defined for this number
-var ErrNoRoute = errors.New("clockwork: no route defined for this number")
+	// ErrNoRoute no route defined for this number
+	ErrNoRoute = errors.New("clockwork: no route defined for this number")
 
-// ErrMissingURL 'URL' parameter not set
-var ErrMissingURL = errors.New("clockwork: 'URL' parameter not set")
+	// ErrMissingURL 'URL' parameter not set
+	ErrMissingURL = errors.New("clockwork: 'URL' parameter not set")
 
-// ErrInvalidSourceIP invalid source IP
-var ErrInvalidSourceIP = errors.New("clockwork: invalid source IP")
+	// ErrInvalidSourceIP invalid source IP
+	ErrInvalidSourceIP = errors.New("clockwork: invalid source IP")
 
-// ErrMissingUDH 'UDH' parameter not specified
-var ErrMissingUDH = errors.New("clockwork: 'UDH' parameter not specified")
+	// ErrMissingUDH 'UDH' parameter not specified
+	ErrMissingUDH = errors.New("clockwork: 'UDH' parameter not specified")
 
-// ErrInvalidServType invalid 'ServType' parameter
-var ErrInvalidServType = errors.New("clockwork: invalid 'ServType' parameter")
+	// ErrInvalidServType invalid 'ServType' parameter
+	ErrInvalidServType = errors.New("clockwork: invalid 'ServType' parameter")
 
-// ErrInvalidExpiryTime invalid 'ExpiryTime' parameter
-var ErrInvalidExpiryTime = errors.New("clockwork: invalid 'ExpiryTime' parameter")
+	// ErrInvalidExpiryTime invalid 'ExpiryTime' parameter
+	ErrInvalidExpiryTime = errors.New("clockwork: invalid 'ExpiryTime' parameter")
 
-// ErrDuplicateClientID duplicate 'ClientID' received
-var ErrDuplicateClientID = errors.New("clockwork: duplicate 'ClientID' received")
+	// ErrDuplicateClientID duplicate 'ClientID' received
+	ErrDuplicateClientID = errors.New("clockwork: duplicate 'ClientID' received")
 
-// ErrInvalidTimeStamp invalid ‘TimeStamp’ parameter
-var ErrInvalidTimeStamp = errors.New("clockwork: invalid ‘TimeStamp’ parameter")
+	// ErrInvalidTimeStamp invalid ‘TimeStamp’ parameter
+	ErrInvalidTimeStamp = errors.New("clockwork: invalid ‘TimeStamp’ parameter")
 
-// ErrInvalidAbsExpiry invalid ‘AbsExpiry’ parameter
-var ErrInvalidAbsExpiry = errors.New("clockwork: invalid ‘AbsExpiry’ parameter")
+	// ErrInvalidAbsExpiry invalid ‘AbsExpiry’ parameter
+	ErrInvalidAbsExpiry = errors.New("clockwork: invalid ‘AbsExpiry’ parameter")
 
-// ErrInvalidDlrType invalid 'DlrType' parameter
-var ErrInvalidDlrType = errors.New("clockwork: invalid 'DlrType' parameter")
+	// ErrInvalidDlrType invalid 'DlrType' parameter
+	ErrInvalidDlrType = errors.New("clockwork: invalid 'DlrType' parameter")
 
-// ErrInvalidConcat invalid 'Concat' parameter
-var ErrInvalidConcat = errors.New("clockwork: invalid 'Concat' parameter")
+	// ErrInvalidConcat invalid 'Concat' parameter
+	ErrInvalidConcat = errors.New("clockwork: invalid 'Concat' parameter")
 
-// ErrInvalidUniqueID invalid 'UniqueID' parameter
-var ErrInvalidUniqueID = errors.New("clockwork: invalid 'UniqueId' parameter")
+	// ErrInvalidUniqueID invalid 'UniqueID' parameter
+	ErrInvalidUniqueID = errors.New("clockwork: invalid 'UniqueId' parameter")
 
-// ErrClientIDRequired client id required. your account is setup to check for a
-// unique client id on every message, one wasn't supplied in this send.
-var ErrClientIDRequired = errors.New("clockwork: 'ClientID' Required - your account is setup to check for a unique client ID on every message, one wasn't supplied in this send")
+	// ErrClientIDRequired client id required. your account is setup to check for a
+	// unique client id on every message, one wasn't supplied in this send.
+	ErrClientIDRequired = errors.New("clockwork: 'ClientID' Required - your account is setup to check for a unique client ID on every message, one wasn't supplied in this send")
 
-// ErrInvalidCharInContent invalid character in 'Content' parameter
-var ErrInvalidCharInContent = errors.New("clockwork: invalid character in 'Content' parameter")
+	// ErrInvalidCharInContent invalid character in 'Content' parameter
+	ErrInvalidCharInContent = errors.New("clockwork: invalid character in 'Content' parameter")
 
-// ErrInvalidTextPayload invalid 'Text' Payload MMS text has an invalid character
-var ErrInvalidTextPayload = errors.New("clockwork: invalid 'TextPayload' MMS text has an invalid character")
+	// ErrInvalidTextPayload invalid 'Text' Payload MMS text has an invalid character
+	ErrInvalidTextPayload = errors.New("clockwork: invalid 'TextPayload' MMS text has an invalid character")
 
-// ErrInvalidHexPayload invalid 'Hex' Payload MMS payload can't be decoded as hex
-var ErrInvalidHexPayload = errors.New("clockwork: invalid 'HexPayload' MMS Payload cant be decoded as hex")
+	// ErrInvalidHexPayload invalid 'Hex' Payload MMS payload can't be decoded as hex
+	ErrInvalidHexPayload = errors.New("clockwork: invalid 'HexPayload' MMS Payload cant be decoded as hex")
 
-// ErrInvalidBase64Payload invalid 'Base64' Payload MMS payload can't be decoded
-// as base64.
-var ErrInvalidBase64Payload = errors.New("clockwork: invalid 'Base64Payload' MMS payload cant be decoded as base64")
+	// ErrInvalidBase64Payload invalid 'Base64' Payload MMS payload can't be decoded
+	// as base64.
+	ErrInvalidBase64Payload = errors.New("clockwork: invalid 'Base64Payload' MMS payload cant be decoded as base64")
 
-// ErrMissingContentType missing content type. No content type provided on MMS
-// payload.
-var ErrMissingContentType = errors.New("clockwork: missing 'ContentType'")
+	// ErrMissingContentType missing content type. No content type provided on MMS
+	// payload.
+	ErrMissingContentType = errors.New("clockwork: missing 'ContentType'")
 
-// ErrMissingID missing 'ID' all MMS payload parts must have an id
-var ErrMissingID = errors.New("clockwork: missing 'ID' all MMS Payload parts must have an ID")
+	// ErrMissingID missing 'ID' all MMS payload parts must have an id
+	ErrMissingID = errors.New("clockwork: missing 'ID' all MMS Payload parts must have an ID")
 
-// ErrMMSMessageTooLarge MMS message too large the combined parts are too large
-// to send
-var ErrMMSMessageTooLarge = errors.New("clockwork: The combined parts are too large to send")
+	// ErrMMSMessageTooLarge MMS message too large the combined parts are too large
+	// to send
+	ErrMMSMessageTooLarge = errors.New("clockwork: The combined parts are too large to send")
 
-// ErrInvalidPayloadID invalid 'Payload' id
-var ErrInvalidPayloadID = errors.New("clockwork: invalid payload ID")
+	// ErrInvalidPayloadID invalid 'Payload' id
+	ErrInvalidPayloadID = errors.New("clockwork: invalid payload ID")
 
-// ErrDuplicatePayloadID duplicate payload id
-var ErrDuplicatePayloadID = errors.New("clockwork: duplicate payload ID")
+	// ErrDuplicatePayloadID duplicate payload id
+	ErrDuplicatePayloadID = errors.New("clockwork: duplicate payload ID")
 
-// ErrNoPayloadOnMMS no payload on MMS
-var ErrNoPayloadOnMMS = errors.New("clockwork: no payload on MMS")
+	// ErrNoPayloadOnMMS no payload on MMS
+	ErrNoPayloadOnMMS = errors.New("clockwork: no payload on MMS")
 
-// ErrDuplicateFileName duplicate 'filename' attribute on payload. All MMS parts
-// must have unique filenames.
-var ErrDuplicateFileName = errors.New("clockwork: duplicate 'filename' attribute on payload -all MMS parts must have unique filenames")
+	// ErrDuplicateFileName duplicate 'filename' attribute on payload. All MMS parts
+	// must have unique filenames.
+	ErrDuplicateFileName = errors.New("clockwork: duplicate 'filename' attribute on payload -all MMS parts must have unique filenames")
 
-// ErrMissingItemID 'ItemId' parameter not specified
-var ErrMissingItemID = errors.New("clockwork: 'ItemId' parameter not specified")
+	// ErrMissingItemID 'ItemId' parameter not specified
+	ErrMissingItemID = errors.New("clockwork: 'ItemId' parameter not specified")
 
-// ErrInvalidItemID invalid 'ItemId' parameter
-var ErrInvalidItemID = errors.New("clockwork: invalid 'ItemId' parameter")
+	// ErrInvalidItemID invalid 'ItemId' parameter
+	ErrInvalidItemID = errors.New("clockwork: invalid 'ItemId' parameter")
 
-// ErrGenerateFileName unable to generate filename for content-type
-var ErrGenerateFileName = errors.New("clockwork: unable to generate filename for 'Content-Type'")
+	// ErrGenerateFileName unable to generate filename for content-type
+	ErrGenerateFileName = errors.New("clockwork: unable to generate filename for 'Content-Type'")
 
-// ErrInvalidCharAction invalid 'InvalidCharAction' parameter
-var ErrInvalidCharAction = errors.New("clockwork: invalid 'InvalidCharAction' parameter")
+	// ErrInvalidCharAction invalid 'InvalidCharAction' parameter
+	ErrInvalidCharAction = errors.New("clockwork: invalid 'InvalidCharAction' parameter")
 
-// ErrInvalidDlrEnroute invalid 'DlrEnroute' parameter
-var ErrInvalidDlrEnroute = errors.New("clockwork: invalid 'DlrEnroute' parameter")
+	// ErrInvalidDlrEnroute invalid 'DlrEnroute' parameter
+	ErrInvalidDlrEnroute = errors.New("clockwork: invalid 'DlrEnroute' parameter")
 
-// ErrInvalidTruncate invalid 'Truncate' parameter
-var ErrInvalidTruncate = errors.New("clockwork: invalid 'Truncate' parameter")
+	// ErrInvalidTruncate invalid 'Truncate' parameter
+	ErrInvalidTruncate = errors.New("clockwork: invalid 'Truncate' parameter")
 
-// ErrInvalidLong invalid 'Long' parameter
-var ErrInvalidLong = errors.New("clockwork: invalid 'Long' parameter")
+	// ErrInvalidLong invalid 'Long' parameter
+	ErrInvalidLong = errors.New("clockwork: invalid 'Long' parameter")
 
-// ErrNoAPIKey no API key provided. You need to provide an API key or a
-// username and password when calling the API.
-var ErrNoAPIKey = errors.New("clockwork: no API key provided you need to provide an API Key or a user name and password when calling the API")
+	// ErrNoAPIKey no API key provided. You need to provide an API key or a
+	// username and password when calling the API.
+	ErrNoAPIKey = errors.New("clockwork: no API key provided you need to provide an API Key or a user name and password when calling the API")
 
-// ErrInvalidAPIKey invalid API key. Log in to your API account to check the key
-// or create a new one
-var ErrInvalidAPIKey = errors.New("clockwork: invalid API key - log in to your API account to check the key or create a new one")
+	// ErrInvalidAPIKey invalid API key. Log in to your API account to check the key
+	// or create a new one
+	ErrInvalidAPIKey = errors.New("clockwork: invalid API key - log in to your API account to check the key or create a new one")
 
-// ErrMustUseAPIKeys account must use API keys. This account isn't allowed to
-// use a username and password, log in to your account to create a key.
-var ErrMustUseAPIKeys = errors.New("clockwork: account must use API keys")
+	// ErrMustUseAPIKeys account must use API keys. This account isn't allowed to
+	// use a username and password, log in to your account to create a key.
+	ErrMustUseAPIKeys = errors.New("clockwork: account must use API keys")
 
-// ErrBlockedSpam blocked by Spam filter. Sometimes your message will be caught
-// by our Spam filter. If you're having trouble because of this error - get in
-// touch.
-var ErrBlockedSpam = errors.New("clockwork: blocked by spam filter - sometimes messages will be caught by our spam filter")
+	// ErrBlockedSpam blocked by Spam filter. Sometimes your message will be caught
+	// by our Spam filter. If you're having trouble because of this error - get in
+	// touch.
+	ErrBlockedSpam = errors.New("clockwork: blocked by spam filter - sometimes messages will be caught by our spam filter")
 
-// ErrInvalidXML invalid XML API post can't be parsed as XML.
-var ErrInvalidXML = errors.New("clockwork: invalid XML API post can't be parsed as XML")
+	// ErrInvalidXML invalid XML API post can't be parsed as XML.
+	ErrInvalidXML = errors.New("clockwork: invalid XML API post can't be parsed as XML")
 
-// ErrInvalidXMLDoc XML document does not validate
-var ErrInvalidXMLDoc = errors.New("clockwork: XML document does not validate")
+	// ErrInvalidXMLDoc XML document does not validate
+	ErrInvalidXMLDoc = errors.New("clockwork: XML document does not validate")
 
-// ErrLongClientID client id too long
-var ErrLongClientID = errors.New("clockwork: Client ID too long")
+	// ErrLongClientID client id too long
+	ErrLongClientID = errors.New("clockwork: Client ID too long")
 
-// ErrRateExceeded query throttling rate exceeded. You've sent too many status
-// requests this hour
-var ErrRateExceeded = errors.New("clockwork: query throttling rate exceeded - you've sent too many status requests this hour")
+	// ErrRateExceeded query throttling rate exceeded. You've sent too many status
+	// requests this hour
+	ErrRateExceeded = errors.New("clockwork: query throttling rate exceeded - you've sent too many status requests this hour")
 
-// ErrStatusCode Clockwork SMS API returned a non 200 HTTP status code
-var ErrStatusCode = errors.New("clockwork: API request did not return HTTP 200 OK")
+	// ErrStatusCode Clockwork SMS API returned a non 200 HTTP status code
+	ErrStatusCode = errors.New("clockwork: API request did not return HTTP 200 OK")
 
-// ErrUnknown if error is not in errorMap then this error will be returned
-var ErrUnknown = errors.New("clockwork: unknown API error code")
+	// ErrUnknown if error is not in errorMap then this error will be returned
+	ErrUnknown = errors.New("clockwork: unknown API error code")
+)
 
 // errorMap maps Clockwork API error codes to error messages. The keys (numbers)
 // are important, they match the API error codes documented here:
